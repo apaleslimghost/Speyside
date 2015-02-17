d800cbb7a84df07eede1d421fa29d19d58cc2f43
Matt Brennan <mattyb1000@gmail.com>
2015-02-17 09:40:17 +0000

    working browser tests

--- test/integration.js
+++ test/integration.js
@@ -1,58 +1,35 @@
+var test = require('tape');
 var sinon = require('sinon');
-var expect = require('sinon-expect').enhance(
-	require('expect.js'), sinon, 'was'
-);
 
 var Speyside = require('../');
 var History = require('html5-history');
 
-var indent = 1;
-function it(label, fn) {
-	indent++;
-	try {
-		fn();
-		console.log(Array(indent).join('  ') + '✓ ' + label);
-	} catch(e) {
-		console.log(Array(indent).join('  ') + '✖ ' + label);
-		console.log('\n' + e.message + '\n' + e.stack + '\n');
-	}
-	indent--;
-}
+test('Speyside integration tests', function(t) {
+	t.test('listen', function (t) {
+		t.test('should call handler with initial state', function(t) {
+			t.plan(3);
 
-function describe(label, fn) {
-	indent++;
-	try {
-		console.log(Array(indent).join('  ') + label);
-		fn();
-	} catch(e) {}
-	indent--;
-}
-
-describe('Speyside integration tests', function() {
-	describe('listen', function () {
-		it('should call handler with initial state', function() {
 			var handler = sinon.spy();
 
 			Speyside.createServer(handler).listen();
 
-			expect(handler).was.called();
-			expect(handler.lastCall.args[0]).to.eql({
-				state: History.getState().data,
-				url: global.location.pathname
-			});
+			t.ok(handler.called, 'handler was called');
+			t.same(handler.lastCall.args[0].state, History.getState().data);
+			t.equal(handler.lastCall.args[0].url, global.location.pathname);
 		});
-		it('should bind history adaptor to window', function() {
+
+		t.test('should bind history adaptor to window', function(t) {
+			t.plan(3);
+
 			var handler = sinon.spy();
 			var state = {};
 
 			Speyside.createServer(handler).listen();
 			History.replaceState(state, null, '/foo');
 
-			expect(handler).was.called();
-			expect(handler.lastCall.args[0]).to.eql({
-				state: state,
-				url: '/foo'
-			});
+			t.ok(handler.called, 'handler was called');
+			t.same(handler.lastCall.args[0].state, state);
+			t.equal(handler.lastCall.args[0].url, '/foo');
 		});
 	});
 });
